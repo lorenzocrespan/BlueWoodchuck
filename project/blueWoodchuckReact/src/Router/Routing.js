5d6ce24d82f23b6fb0c35455dfde14aef36d723e
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-02-03 10:23:40 +0100

    Fix routing

--- project/blueWoodchuckReact/src/Router/Routing.js
+++ project/blueWoodchuckReact/src/Router/Routing.js
@@ -1,13 +1,36 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Outlet, Link  } from "react-router-dom";
 import LoginView from "../Components/LoginView";
-import HomepageView from "../Components/HomepageView";
+import HomepageView from "../Components/FunctionView/HomepageView";
+import Header from "../Components/BaseComponent/Header";
+import Footer from "../Components/BaseComponent/Footer";
+import Link1 from "../Components/FunctionView/Link1";
+
+
+const Layout = () => {
+    return (
+      <>
+        <Header />
+  
+        <Outlet />
+
+        <Footer />
+      </>
+    )
+  };
+
+// <Route path="*" element={<NoPage />} />
 
 export default function Routing() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LoginView />} />
-                <Route path="/Homepage" element={<HomepageView />} />
+                <Route path="/" element={<Layout />}>
+                    <Route path="/Homepage" element={<HomepageView />} />
+                    <Route path="/link" element={<Link1 />} />
+                    
+                </Route>
+
             </Routes>
         </Router>
     )
