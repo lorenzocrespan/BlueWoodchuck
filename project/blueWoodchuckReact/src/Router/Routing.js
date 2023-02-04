1700a09718ff989e8e42e8a9f05a2d555a96919c
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-02-04 19:27:58 +0100

    Add 404 page

--- project/blueWoodchuckReact/src/Router/Routing.js
+++ project/blueWoodchuckReact/src/Router/Routing.js
@@ -1,37 +1,36 @@
-import { BrowserRouter as Router, Route, Routes, Outlet, Link  } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Outlet, Link } from "react-router-dom";
 import LoginView from "../Components/LoginView";
 import HomepageView from "../Components/FunctionView/HomepageView";
 import Header from "../Components/BaseComponent/Header";
 import Footer from "../Components/BaseComponent/Footer";
 import Link1 from "../Components/FunctionView/Link1";
 import NewForm from "../Components/FunctionView/NewForm";
+import View404 from "../Components/404View";
 
 const Layout = () => {
-    return (
-      <>
-        <Header />
-  
-        <Outlet />
+  return (
+    <>
+      <Header />
 
-        <Footer />
-      </>
-    )
-  };
+      <Outlet />
 
-// TODO: Add <Route path="*" element={<NoPage />} />
+      <Footer />
+    </>
+  )
+};
 
 export default function Routing() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LoginView />} />
-                <Route path="/" element={<Layout />}>
-                    <Route path="/Homepage" element={<HomepageView />} />
-                    <Route path="/link" element={<Link1 />} />
-                    <Route path="/newForm" element={<NewForm />} />
-                </Route>
-
-            </Routes>
-        </Router>
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginView />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/Homepage" element={<HomepageView />} />
+          <Route path="/link" element={<Link1 />} />
+          <Route path="/newForm" element={<NewForm />} />
+        </Route>
+        <Route path="*" element={<View404 />} />
+      </Routes>
+    </Router>
+  )
 }
