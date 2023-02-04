f1c806e72fe14e7f44c0c2656c9709d218f651b9
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-02-04 15:33:03 +0100

    Add metamask handler

--- project/blueWoodchuckReact/src/index.js
+++ project/blueWoodchuckReact/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { MetaMaskProvider } from 'metamask-react';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-        <App />
+        <MetaMaskProvider>
+                <App />
+        </MetaMaskProvider>
 );
