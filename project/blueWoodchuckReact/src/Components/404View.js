cd778307ead199f9d50897318c036827e634f282
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-02-05 16:57:54 +0100

    Redirect button to login page

--- project/blueWoodchuckReact/src/Components/404View.js
+++ project/blueWoodchuckReact/src/Components/404View.js
@@ -1,6 +1,16 @@
+import {useNavigate} from "react-router-dom";
+
 function View404() {
+
+    const navigator = useNavigate()
+
+    async function redirectDefaultPage() {
+        // TODO: Redirect to login page if user is not logged in else redirect to homepage
+        navigator("/")
+    }
+
     return (
-        <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
+        <section className="flex items-center h-screen p-16 dark:bg-gray-900 dark:text-gray-100">
             <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                 <div className="max-w-md text-center">
                     <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
@@ -8,7 +18,7 @@ function View404() {
                     </h2>
                     <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
                     <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-                    <a rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</a>
+                    <button className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900" onClick={redirectDefaultPage}>Back</button>
                 </div>
             </div>
         </section>
