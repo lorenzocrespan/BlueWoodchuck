109accd69e6aa373b75e4075d229cc5d539a1a50
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-02-09 15:19:43 +0100

    Fix load contract for user

--- project/blueWoodchuckReact/src/Components/FunctionView/ContractEntry.js
+++ project/blueWoodchuckReact/src/Components/FunctionView/ContractEntry.js
@@ -1,13 +1,30 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+
 function ContractEntry(props) {
+
+    const [dataForm, setDataForm] = useState({});
+
+    useEffect(() => {
+        getDataForm();
+    }, []);
+
+
+    const getDataForm = async () => {
+        // Get form data for each contract.
+        let dataFormToSet = await props.FormContract.methods.readFormAddress(props.id).call();
+        setDataForm(dataFormToSet);
+    }
 
 
     return (
         <li>
             <article>
                 <a rel="noopener noreferrer" href="#" className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900">
-                    <h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">Earum at ipsa aliquid quis, exercitationem est. {props.id}</h3>
-                    <time className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400">Oct 13, 2020</time>
-                    <p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda odit, velit eveniet est.</p>
+                    <h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">Nome caso: {dataForm.caseName}</h3>
+                    <h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">Contratto: {props.id}</h3>
+                    <time className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400">{dataForm.date ? new Date(dataForm.date* 1000).toLocaleDateString("it-IT") : "N/A"}</time>
+                    <p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">{dataForm.contentDescription ? dataForm.contentDescription : "N/A"}</p>
                 </a>
             </article>
         </li>
--- project/blueWoodchuckReact/src/Components/FunctionView/ListContract.js
+++ project/blueWoodchuckReact/src/Components/FunctionView/ListContract.js
@@ -1,33 +1,31 @@
 import ContractEntry from "./ContractEntry";
 import React, { useState, useEffect } from 'react';
 
+
 function ListContract(props) {
 
     const isConsoleActive = true;     // Enable/Disable console debug.
 
-    const [inputList, setInputList] = useState([]);
     const [contractsInCharge, setContractsInCharge] = useState([]);
 
     useEffect(() => {
-        if(props.account !== undefined) getUserFormsAddresses();
+        if (props.account !== undefined) getUserFormsAddresses();
     }, [props.account]);
 
-    const onAddBtnClick = event => {
-        setInputList(inputList.concat(
-            <ContractEntry
-                key={inputList.length}
-                id={inputList.length}
-            />));
-    };
-
+    const listItems = contractsInCharge.map((data, index) =>
+        <ContractEntry
+            key={index}
+            FormContract={props.FormContract}
+            id={data}
+        />
+    );
 
     // Obtain the list of contracts in charge for the current user.
     const getUserFormsAddresses = async () => {
         // Get the list of contracts in charge.
-        console.debug(props.FormContract.methods);
-        console.debug(props.account);
         const contractsInCharge = await props.FormContract.methods.getUserFormAddresses(props.account).call();
         if (isConsoleActive) console.debug("Contracts in charge: ", contractsInCharge);
+
         // Set the list of contracts in charge.
         setContractsInCharge(contractsInCharge);
     }
@@ -35,10 +33,7 @@ function ListContract(props) {
     return (
         <ul className="flex flex-col container justify-between mx-auto p-4 lg:p-8 dark:bg-gray-800 dark:text-gray-100">
             <h2 className="mb-4 text-2xl font-semibold">Lista contratti in carico</h2>
-            <button onClick={onAddBtnClick}>
-                Add User
-            </button>
-            {inputList}
+            {listItems}
         </ul>
     )
 }
