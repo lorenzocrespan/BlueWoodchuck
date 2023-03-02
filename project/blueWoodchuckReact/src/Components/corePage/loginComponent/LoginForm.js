b341281e93e390e4212e08c2e146cef489751e32
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-03-02 01:29:23 +0100

    Login refactor

--- project/blueWoodchuckReact/src/Components/corePage/loginComponent/LoginForm.js
+++ project/blueWoodchuckReact/src/Components/corePage/loginComponent/LoginForm.js
@@ -6,11 +6,13 @@ import ErrorPopup from "../../popupComponent/ErrorPopup";
 
 function LoginForm() {
 
-    const isConsoleActive = true;                           // Enable/Disable console debug.
-
-    const navigator = useNavigate();                        // Variable to navigate to another page.
-    const [errorPopup, setErrorPopup] = useState(false);    // Variable to show the popup modal.
-    const [title, setTitle] = useState("Titolo del popup"); // Variable to customize the title of the popup modal.
+    // Enable/Disable console debug.
+    const isConsoleActive = false;
+    // Variable to navigate to another page.
+    const navigator = useNavigate();
+    // Variable to set the popup modal (show, title and body).            
+    const [errorPopup, setErrorPopup] = useState(false);
+    const [title, setTitle] = useState("Titolo del popup");
     const [body, setBody] = useState("Corpo del popup");
 
     // Function to connect to MetaMask.
@@ -31,16 +33,10 @@ function LoginForm() {
                 // If the connection is successful, navigate to the homepage.
                 navigator("/homepage");
             })
-            .catch((err) => {
-                if (err.code === -32002) {
-                    setTitle("EIP-1193 userRejectedRequest error");
-                    setBody("Accesso negato. Per accedere devi autorizzare MetaMask.");
-                    setErrorPopup(true);
-                } else {
-                    setTitle("Errore di connessione");
-                    setBody("Si è verificato un errore durante la connessione a MetaMask. Riprova più tardi.");
-                    setErrorPopup(true);
-                }
+            .catch(() => {
+                setTitle("Errore di connessione");
+                setBody("Si è verificato un errore durante la connessione a MetaMask. Riprova più tardi.");
+                setErrorPopup(true);
             });
     }
 
@@ -50,22 +46,22 @@ function LoginForm() {
     };
 
     return (
-        <div className="w-fit h-fit flex rounded bg-white bg-opacity-60">
+        <div className="w-fit h-fit font-semibold flex rounded bg-white bg-opacity-60">
             <ErrorPopup
                 onClose={popupCloseHandler}
                 errorPopup={errorPopup}
                 title={title}
                 body={body}
             />
-            <div name="leftSideLoginForm" className="w-1/2 h-full flex p-4 rounded-l bg-white">
+            <div className="w-1/2 h-full flex p-4 rounded-l bg-white">
                 <img src={require("../../../Asset/Images/blueWoodchuckLogo.jpg")} alt="blueWoodchuckLogo" />
             </div>
-            <div name="rightSideLoginForm" className="w-1/2 h-full flex flex-col p-4 rounded-r self-center text-center">
-                <p name="title" className="pb-12 text-3xl font-bold">
+            <div className="w-1/2 h-full flex flex-col p-4 rounded-r self-center text-center">
+                <p name="title" className="pb-12 text-3xl">
                     <span className="text-blue-600">Blue</span>
                     <span className="text-amber-600">Woodchuck</span>
                 </p>
-                <button className="w-1/2 h-12 font-semibold rounded-md self-center text-white bg-blue-800 hover:bg-amber-600  ease-out duration-500" onClick={login}> Login con Metamask</button>
+                <button className="w-1/2 h-12 rounded-md self-center text-white bg-blue-800 hover:bg-amber-600  ease-out duration-500" onClick={login}> Login con Metamask</button>
             </div>
         </div>
     );
