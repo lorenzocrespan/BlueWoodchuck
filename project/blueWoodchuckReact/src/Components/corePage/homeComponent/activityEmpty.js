f055c66e6329e228d9046581519a72b10b91e91e
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-03-14 20:58:26 +0100

    Init fix activity section

---
+++ project/blueWoodchuckReact/src/Components/corePage/homeComponent/activityEmpty.js
@@ -0,0 +1,10 @@
+function EmptyActivity(props) {
+
+    return (
+        <div className="flex mx-auto justify-center p-10 rounded-md">
+            <p className="text-xl">{props.textSection}</p>
+        </div>
+    )
+}
+
+export default EmptyActivity;
--- project/blueWoodchuckReact/src/Components/corePage/homeComponent/activitySection.js
+++ project/blueWoodchuckReact/src/Components/corePage/homeComponent/activitySection.js
@@ -1,18 +1,21 @@
-import { useEffect } from 'react';                    // React hooks.
+// Import - React
+import { useEffect, useState } from 'react';
+// Import - Components
+import EmptyActivity from "./activityEmpty";
 import { BodyEntry, TitleEntry } from "./activityEntry";
-import { useState } from "react";
-
+// Import - Supabase
 import { createClient } from '@supabase/supabase-js'
-
 
 function RecentActivity() {
 
-    const [inputList, setInputList] = useState([]);
-
+    // Enable/Disable console debug.
+    const isConsoleActive = false;
+    // Supabase connection variables.
     const supabaseUrl = 'https://gnvpuxxfihgmnzydwzdn.supabase.co'
     const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdudnB1eHhmaWhnbW56eWR3emRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY5MTg0NTQsImV4cCI6MTk5MjQ5NDQ1NH0.NprpJMRf9iscSwBEP9CJCW2TeehdTSgol4uNG_SvbgY"
     const supabase = createClient(supabaseUrl, supabaseKey)
-
+    // Variable to store the list of activities from the database.
+    const [inputList, setInputList] = useState([]);
 
     useEffect(() => {
         getIDsupabase();
@@ -20,22 +23,14 @@ function RecentActivity() {
 
     const getIDsupabase = async () => {
         try {
-            let { data: Activity, error } = await supabase
-                .from('Activity')
-                .select('*')
-
-            if (error) {
-                throw error
-            }
-
+            let { data: Activity, error } = await supabase.from('Activity').select('*')
+            if (error) throw error
             if (Activity) {
                 console.log("Activity")
                 console.log(Activity)
             }
         } catch (error) {
             console.log('error', error.message)
-        } finally {
-            console.log("finally")
         }
     }
 
@@ -49,19 +44,25 @@ function RecentActivity() {
     };
 
     return (
-        <div className="container mx-auto sm:p-4  text-gray-100 bg-blue-900 p-10 rounded-md">
+        <div className="container mx-auto text-gray-100 bg-blue-900 p-12 rounded-md">
             <h2 className="mb-4 text-2xl font-semibold leading-tight">Attività recenti</h2>
-
-            <div className="overflow-x-auto">
-                <table className="min-w-full text-xs">
-                    <thead className=''>
-                        <TitleEntry />
-                    </thead>
-                    <tbody>
-                        {inputList}
-                    </tbody>
-                </table>
-            </div>
+            {
+                inputList.length === 0 ?
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full text-xs">
+                            <thead className=''>
+                                <TitleEntry />
+                            </thead>
+                            <tbody>
+                                {inputList}
+                            </tbody>
+                        </table>
+                    </div>
+                    :
+                    <EmptyActivity
+                        textSection="Non è stata trovata alcuna attività recente."
+                    />
+            }
         </div>
     )
 }
